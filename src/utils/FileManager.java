02ea3b6b91955c3d66ce215a6a0c2ce8cb366f24
Bijan Eisapour <bijaneisapour@gmail.com>
2021-01-13 11:09:58 +0330

    [UPDATE] FileManager & Main

--- src/utils/FileManager.java
+++ src/utils/FileManager.java
@@ -26,12 +26,36 @@ public class FileManager {
 		return false;
 	}
 	
-	public String read() {
+	public boolean writeLine(String content, boolean append) {
+		return this.write(content + "\n", append);
+	}
+	
+	public String readAll() {
 		BufferedReader reader;
 		
 		try {
 			reader = new BufferedReader(new FileReader(path));
-			String content = reader.lines().collect(Collectors.joining());
+			String content = reader.lines().collect(Collectors.joining("\n"));
+			reader.close();
+			
+			return content;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	public String readLine(int lineNumber) {
+		BufferedReader reader;
+		
+		try {
+			reader = new BufferedReader(new FileReader(path));
+			
+			String content = null;
+			for (int i = 0; i < lineNumber; i++)
+				content = reader.readLine();
+			
 			reader.close();
 			
 			return content;
